84761a2bad4ce7e4b647fe3ffcae773d3515d452
Lance Douglas <ldouglastech@gmail.com>
2016-09-05 22:07:07 -0400

    FirstProgram3 is complete.

--- src/FirstProgram3.java
+++ src/FirstProgram3.java
@@ -1,11 +1,18 @@
+import java.util.Scanner;
+
 /**
  * Created by parkway on 9/5/16.
  */
 public class FirstProgram3 {
     public static void main(String []args) {
-        System.out.println("Hello out there." +
-                "I will add three numbers for you." +
-                "Enter three numbers on a line:");
-
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Hello out there.\n" +
+                "I will add three numbers for you.\n" +
+                "Enter three whole numbers on a line:");
+        int i = scn.nextInt();
+        int j = scn.nextInt();
+        int f = scn.nextInt();
+        int sum = i + j + f;
+        System.out.println("The sum of those three numbers is \n" + sum);
     }
 }
