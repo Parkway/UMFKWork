74ed59dd49e55139f30ed9bdcfb9f46c60fc8448
Lance Douglas <ldouglastech@gmail.com>
2016-09-08 10:02:21 -0400

    Added comments to describe code in FP3, formalized variable names.

--- src/FirstProgram3.java
+++ src/FirstProgram3.java
@@ -1,27 +1,26 @@
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.util.InputMismatchException; // Exception handling
+import java.util.Scanner; // Adds Scanner for basic IO
+/* File name: FirstProgram3.java
+    This program takes three integers from the user and adds them together, and returns the sum as output.
+    For example, if I enter 5, 10, and 5, the program returns 20.
+    Lance Douglas, FirstProgram3, 9/7/16
+*/
 
-
-/**
- * Created by parkway on 9/5/16.
- */
-public class FirstProgram3 {
-    public static void main(String []args) {
-        Scanner scn = new Scanner(System.in);
+public class FirstProgram3 { //Identifies class name
+    public static void main(String []args) { // Method name
+        Scanner scn = new Scanner(System.in); // Scanner, allows for user input.
         System.out.println("Hello out there.\n" +
                 "I will add three numbers for you.\n" +
-                "Enter three whole numbers on a line:");
-        try
-        {
-            int i = scn.nextInt();
-            int j = scn.nextInt();
-            int f = scn.nextInt();
-            int sum = i + j + f;
-            System.out.println("The sum of those three numbers is \n" + sum);
-        }
-        catch(InputMismatchException exception)
-        {
+                "Enter three whole numbers on a line:"); // What the program displays.
+        try {
+            int firstNum = scn.nextInt(); // The first number
+            int secondNum = scn.nextInt(); // The second number
+            int thirdNum = scn.nextInt(); // The third number
+            int sum = firstNum + secondNum + thirdNum; // Adds the three numbers
+            System.out.println("The sum of those three numbers is \n" + sum); // Displays the sum.
+            }
+        catch(InputMismatchException exception) {
             System.out.println("This is not an integer, please try again.");
-        }
+        } // try / catch is to catch any errors if the user enters something that isn't an integer.
     }
 }
--- src/VendingChange.java
+++ src/VendingChange.java
@@ -1,21 +1,23 @@
-import java.util.Scanner;
+import java.util.Scanner; // Basic user IO
 
-/**
- * Created by parkway on 9/5/16.
- */
-public class VendingChange {
-    public static void main (String[] args) {
+public class VendingChange { // Class name
+    public static void main (String[] args) { // Method name
 
-        int quarters;
-        int dimes;
-        int nickels;
-        
-        double dollars;
-
-        Scanner scn = new Scanner(System.in);
-        System.out.println("Enter price of item, \n From 25 cents to $1");
+        Scanner scn = new Scanner(System.in); // For user input
+        System.out.println("Enter price of item, \nFrom 25 cents to $1");
         int price = scn.nextInt();
+        int answer = 100 - price;
+
+        int quarters = answer / 25;
+        int dimes = answer / 10 ;
+        int nickels = answer / 5;
+
+
+
         System.out.println("You bought an item for " + price + " cents and gave me a dollar.\n" +
-                "Your change is:\n");
+                "Your change is:\n" +
+                quarters + " quarters, " +
+                dimes + " dimes, and " +
+                nickels + " nickels.");
     }
 }
