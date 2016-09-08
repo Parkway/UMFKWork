ccfa41087afbda09e3791e247a6ec63dd2a0c10a
Lance Douglas <ldouglastech@gmail.com>
2016-09-08 10:36:08 -0400

    Oops broke FP3

--- src/FirstProgram3.java
+++ src/FirstProgram3.java
@@ -3,7 +3,7 @@ import java.util.Scanner; // Adds Scanner for basic IO
 /* File name: FirstProgram3.java
     This program takes three integers from the user and adds them together, and returns the sum as output.
     For example, if I enter 5, 10, and 5, the program returns 20.
-    Lance Douglas, FirstProgram3, 9/7/16
+    Lance Douglas, FirstProgram3, September 7, 2016
 */
 
 public class FirstProgram3 { //Identifies class name
@@ -18,7 +18,7 @@ public class FirstProgram3 { //Identifies class name
             int thirdNum = scn.nextInt(); // The third number
             int sum = firstNum + secondNum + thirdNum; // Adds the three numbers
             System.out.println("The sum of those three numbers is \n" + sum); // Displays the sum.
-            }
+        }
         catch(InputMismatchException exception) {
             System.out.println("This is not an integer, please try again.");
         } // try / catch is to catch any errors if the user enters something that isn't an integer.
