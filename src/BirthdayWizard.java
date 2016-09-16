ff9c3e14a41716d7c1cdf47d7c5a210624e791e7
Lance Douglas <ldouglastech@gmail.com>
2016-09-16 14:37:34 -0400

    BirthdayWizard typo fixed.

--- src/BirthdayWizard.java
+++ src/BirthdayWizard.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
   Lance Douglas, BirthdayWizard, September 16, 2016. */
 public class BirthdayWizard {
   public static void main(String[] args) {
-    System.out.println("Greetings.\n What year were you born?");
+    System.out.println("Greetings.\nWhat year were you born?");
     Scanner scn = new Scanner(System.in);
     int birthYear = scn.nextInt(); //Year of birth.
     System.out.println("Choose any age.");
     int age = scn.nextInt(); //The age in which you want to find out when you will become. 
-    System.out.println("You will turn " + age + "in the year");
+    System.out.println("You will turn " + age + " in the year");
     int ageInYear = birthYear + age; //Adds age to birthyear to get answer.
     System.out.println(ageInYear);
   }
