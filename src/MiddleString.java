a3223d444e720e4b4804a2a950921314303728de
Lance Douglas <ldouglastech@gmail.com>
2016-10-01 13:53:09 -0400

    Apparently I can't read instructions, and MiddleString is supposed to sort lexicographically. Fixed.

--- src/MiddleString.java
+++ src/MiddleString.java
@@ -1,8 +1,6 @@
+import java.util.Arrays;
 import java.util.Scanner;
-/*This returns the second word typed in a list of three words.
-Unfortunately, it is static and won't work in other cases where there is more than three words.
-I hard coded it to capture and return the second word.
-I hope this is the way it is supposed to be done, because it looks kinda gross to me.
+/*This program returns the second lexicographic word in a list of three words.
 Lance Douglas, MiddleString, September 12, 2016.
  */
 public class MiddleString {
@@ -10,8 +8,8 @@ public class MiddleString {
         System.out.println("Please enter three words separated by spaces.");
         Scanner scn = new Scanner(System.in); //Accepts user input
         String threeWords = scn.nextLine(); //Turns user input into variable. Nothing new. String is just text IO instead of number IO.
-        String words[] = threeWords.split(" "); //New stuff! words[] counts words in variable. threeWords is userinputvariable, .split(" ") splits the String at each " " space.
-        String middleWord = words[1]; // Returns second word in the words[] array. No idea how to get middle specifically. Google returned middle char. Not word. Not helpful.
-        System.out.println("The middle word is " + middleWord);
+        String words[] = threeWords.split(" ");//New stuff! words[] counts words in variable. threeWords is userinputvariable, .split(" ") splits the String at each " " space.
+        Arrays.sort(words);
+        System.out.println("The middle word is " + words[1]);
     }
 }
