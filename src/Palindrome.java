c5314e40fe2b98e9184d8186bc9ac3e23c8d4023
Lance Douglas <ldouglastech@gmail.com>
2016-10-17 12:14:19 -0400

    Commented more.

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -14,7 +14,7 @@ public class Palindrome {
                 "\nEnter text (Just letters and blanks)");
         String original = scn.nextLine().toLowerCase(); //String original means input word; nextLine().toLowerCase() means ignore case, so 'RACecaR' is still correct.
 
-        int length = original.length() - 1; //-1 because it
+        int length = original.length() - 1; //-1 because I get IndexOutOfBounds errors. This fixes it.
 
         for ( int charLocation = length; charLocation >= 0; charLocation-- ) { //i = length of word, while i is greater than or equal to 0, i--
             reverse = reverse + original.charAt(charLocation); //Original - backwards + original word, one letter at a time. charAt(int) means "what is the char at this index"
@@ -39,7 +39,6 @@ public class Palindrome {
             print("Please enter 'y' or 'n' to answer.");
             repeat();
         }
-
     }
     private static void print(String s) {
         System.out.println(s); //2lazy4System.out.println
