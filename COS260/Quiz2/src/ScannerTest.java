9c2c7a15033ebfb0cb12d9184687ef3b1ebce548
Lance Douglas <ldouglastech@gmail.com>
2016-10-22 12:18:48 -0400

    Added Quiz2 and stuff

---
+++ COS260/Quiz2/src/ScannerTest.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ScannerTest {
+    public static void main(String[] args) {
+        System.out.println("Enter the first string.");
+        Scanner scn = new Scanner(System.in);
+        String first = scn.next();
+        System.out.println("Enter second string.");
+        String second = scn.next();
+        String concat = first + " " + second; //Won't work, placeholder. Temporary.
+        System.out.println("The first string is '" + first + "', it is " + first.length()+ " characters long.\n"
+                + "The second string is '" + second + "', it is " + second.length() + " characters long.\n"
+                + "The strings combined is '" + concat + "', it is " + concat.length() + " characters long.");
+    }
+}
