8c4c8989550b87a4155c4f8497a42e9ec4e988c5
Lance Douglas <ldouglastech@gmail.com>
2016-12-06 22:36:58 -0500

    Why is broken??

--- COS260/HMWK7/src/SortedIntegerNoDuplicates.java
+++ COS260/HMWK7/src/SortedIntegerNoDuplicates.java
@@ -5,43 +5,42 @@ public class SortedIntegerNoDuplicates {
     public static void main(String[] args) throws IOException {
         writer();
     }
-
-    static void writer() throws IOException {
+    //For reference, FN is File Name
+    private static void writer() throws IOException {
         boolean firstNum = true;
         Scanner scn = new Scanner(System.in);
-        //gives instructions for the user
-        System.out.println("Enter input file name:");
-        String originalFileName = scn.next();
-        System.out.println("Enter output file name:");
-        String newFileName = scn.next();
+        System.out.print("Enter input file name:");
+        String originalFN = scn.next();
+        System.out.print("Enter output file name:");
+        String newFN = scn.next();
         try {
-            File file = new File(newFileName);
-            PrintWriter print = new PrintWriter((new FileWriter(file)));
-            Scanner fileScanner = new Scanner(new File(originalFileName));
+            File file = new File(newFN);                                    //W
+            PrintWriter PW = new PrintWriter((new FileWriter(file)));       //T
+            Scanner fScan = new Scanner(new File(originalFN));              //F
 
             int temp = 0;
-            while (fileScanner.hasNextLine()){
-                String line = fileScanner.nextLine();
+            while (fScan.hasNextLine()){
+                String line = fScan.nextLine();
                 int number = Integer.parseInt(line);
                 if(firstNum){
-                    print.write(line);
-                    if (fileScanner.hasNextLine()) {
-                        print.println();
+                    PW.write(line);
+                    if (fScan.hasNextLine()) {
+                        PW.println();
                     }
                     temp = number;
                     firstNum = false;
                 }
                 else if (temp!= number){
-                    print.write(line);
-                    if (fileScanner.hasNextLine())
-                        print.println();
+                    PW.write(line);
+                    if (fScan.hasNextLine())
+                        PW.println();
                     temp = number;
                 }
             }
             //closes scanners and print writers
             scn.close();
-            print.close();
-            fileScanner.close();
+            PW.close();
+            fScan.close();
             System.out.println("Numbers from input file");
             System.out.println("copied to output file with");
             System.out.println("duplicates removed.");
