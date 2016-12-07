43806ba143b527b34a96c4a1e2d87d06ee018fc6
Lance Douglas <ldouglastech@gmail.com>
2016-12-06 23:10:38 -0500

    iS NOT BROKEN

--- COS260/HMWK7/src/SortedIntegerNoDuplicates.java
+++ COS260/HMWK7/src/SortedIntegerNoDuplicates.java
@@ -1,34 +1,35 @@
-import java.io.*;
+import java.io.*; //New thing!? Necessary components for FS I/O.
 import java.util.*;
 
-public class SortedIntegerNoDuplicates {
-    public static void main(String[] args) throws IOException {
+public class SortedIntegerNoDuplicates { //Disgusting class name.
+    public static void main(String[] args) throws IOException { //Throws exception in method called by main method. Why?  ¯\_(ツ)_/¯
         writer();
     }
     //For reference, FN is File Name
-    private static void writer() throws IOException {
-        boolean firstNum = true;
+    //In my *nix world, FS is innately understood to be File System, so F* means File Something.
+    private static void writer() throws IOException { //See line five.
+        boolean firstNum = true; //Needed a boolean for T/F
         Scanner scn = new Scanner(System.in);
         System.out.print("Enter input file name:");
         String originalFN = scn.next();
         System.out.print("Enter output file name:");
         String newFN = scn.next();
-        try {
-            File file = new File(newFN);                                    //W
-            PrintWriter PW = new PrintWriter((new FileWriter(file)));       //T
-            Scanner fScan = new Scanner(new File(originalFN));              //F
+        try { //For exception handling. This shouldn't be new.
+            File file = new File(newFN);                                    //W     Make a new file named the value assigned to variable newFN.
+            PrintWriter PW = new PrintWriter((new FileWriter(file)));       //T     Introduce a way to output to previously made file.
+            Scanner fScan = new Scanner(new File(originalFN));              //F     Scanner to read input file.
 
-            int temp = 0;
-            while (fScan.hasNextLine()){
+            int temp = 0; //Temporary number.
+            while (fScan.hasNextLine()){ //"Scan file to read next line."
                 String line = fScan.nextLine();
-                int number = Integer.parseInt(line);
+                int number = Integer.parseInt(line); //Because numbers.txt -> String to integer.
                 if(firstNum){
-                    PW.write(line);
+                    PW.write(line); //PrintWriter writes it to output file.
                     if (fScan.hasNextLine()) {
                         PW.println();
                     }
                     temp = number;
-                    firstNum = false;
+                    firstNum = false; //Moral of the story, if num = last num, it's a duplicate and therefore must DIE. :)
                 }
                 else if (temp!= number){
                     PW.write(line);
@@ -40,14 +41,14 @@ public class SortedIntegerNoDuplicates {
             //closes scanners and print writers
             scn.close();
             PW.close();
-            fScan.close();
-            System.out.println("Numbers from input file");
-            System.out.println("copied to output file with");
-            System.out.println("duplicates removed.");
+            fScan.close();  //Told the possible buffer overflow to go f*ck itself! Wait does Java have buffer overflow problems?
+            System.out.println("Numbers from input file"
+                + "copied to output file with"
+                + "duplicates removed.");
         }
         catch(FileNotFoundException e){
             System.out.println("The file is not found. Let's try again.");
-            writer();
+            writer(); //Restarts method. Recursion. Neato!
         }
     }
 }
