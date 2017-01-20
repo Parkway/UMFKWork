16b30047e3a99a465d6494e2d80a8e140032afce
Lance Douglas <ldouglastech@gmail.com>
2017-01-20 09:20:00 -0500

    Got some more done, separated P1 and P2, P2 complete, needing randomization technique which doesn't repeat any sequences.

--- COS312/HMWK1/src/Quotes.java
+++ COS312/HMWK1/src/Quotes.java
@@ -1,96 +1,53 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.util.Collections;
 
-class Window extends JFrame
-{
-    JPanel frame = new JPanel();
-
-    String[] author = {"Select Choice","Nelson Mandela","Shawn Ashmore","John Richardson","Ben Franklin","Lance Douglas"};
-    String[] quotes = {"Quote Goes Here","Education is the most powerful weapon which you can use to change the world.",
-            "Style is a reflection of your attitude and your personality.","They can be silly, they can be political, whatever.",
-            "An investment in knowledge pays the best interest.","I just spent way too much money. Why did I do that?"};
-
-    JLabel lbl = new JLabel();
-
-    JComboBox Spinner = new JComboBox(author);
-
-    private Window() {
-        super("Quotes");
-        setSize(600, 250);
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        add(frame);
-        setVisible(true);
-        Color customColor = new Color(2,84,82);
-        setBackground(customColor);
-
-        Spinner.setSelectedIndex(0);
-        frame.add(Spinner, BorderLayout.NORTH);
-        frame.setPreferredSize(new Dimension(640, 480));
-        frame.add(lbl, BorderLayout.SOUTH);
-
-        lbl.setHorizontalTextPosition(JLabel.CENTER);
-        lbl.setVerticalTextPosition(JLabel.CENTER);
+public class Quotes {
+    public static void main(String[] args) throws InterruptedException {
 
 
-        Spinner.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent event) {
+        String quotesList[] = {"Education is the most powerful weapon which you can use to change the world.\n\t\t- Nelson Mandela",
+                "They can be silly, they can be political, whatever.\n\t\t- John Richardson",
+                "An investment in knowledge pays the best interest.\n\t\t- Ben Franklin",
+                "Style is a reflection of your attitude and your personality.\n\t\t- Shawn Ashmore",
+                "I just spent way too much money. Why did I do that?\n\t\t- Lance Douglas"};
 
-                JComboBox comboBox = (JComboBox) event.getSource();
-
-                Object selected = comboBox.getSelectedItem();
-                if(selected.toString().equals(author[0]))
-                    lbl.setText(quotes[0]);
-                else if(selected.toString().equals(author[1]))
-                    lbl.setText(quotes[1]);
-                else if(selected.toString().equals(author[2]))
-                    lbl.setText(quotes[2]);
-                else if(selected.toString().equals(author[3]))
-                    lbl.setText(quotes[3]);
-                else if(selected.toString().equals(author[4]))
-                    lbl.setText(quotes[4]);
-                else if(selected.toString().equals(author[5]))
-                    lbl.setText(quotes[5]);
-                else
-                    lbl.setText("You shouldn't be seeing this error message. If you are, contact the dev.");
-
-            }
-        });
-
-        setVisible(true);
+        int quoteNum = quotesList.length;
+        while (quoteNum > 0) {
+            int quoteChoice = (int) (Math.random() * quoteNum);
+            print(quotesList[quoteChoice]);
+            quoteNum--;
+            Thread.sleep(1500);
+        }
     }
-
-    public static void main (String[] args) {
-        Window gui = new Window();
+    private static void print(String s) {
+        System.out.println(s);
     }
 }
 
+/*print("Education is the most powerful weapon which you can use to change the world." +
+            "\n                 - Nelson Mandela\n"); //Also, I know how to use \" to put quotation marks in the text, but the - Name format doesn't require any.
 
-   /* String nMandela = "Education is the most powerful weapon which you can use to change the world." +
-            "\n                 - Nelson Mandela\n"; //Also, I know how to use \" to put quotation marks in the text, but the - Name format doesn't require any.
+        Thread.sleep(1500); //Delays code execution for effect. No getting spammed with quotes.
 
-    Thread.sleep(1500); //Delays code execution for effect. No getting spammed with quotes.
+        print("They can be silly, they can be political, whatever." +
+            "\n                 - John Richardson\n");
 
-    String jRichardson = "They can be silly, they can be political, whatever." +
-            "\n                 - John Richardson\n";
+        Thread.sleep(1500);
 
-    Thread.sleep(1500);
+        print("They can be silly, they can be political, whatever." +
+            "\n                 - Benjamin Franklin\n");
 
-    String bFranklin = "An investment in knowledge pays the best interest." +
-            "\n                 - Benjamin Franklin\n";
+        Thread.sleep(1500);
 
-    Thread.sleep(1500);
+        print("Style is a reflection of your attitude and your personality." +
+            "\n                 - Shawn Ashmore\n");
 
-    String sAshmore = "Style is a reflection of your attitude and your personality." +
-            "\n                 - Shawn Ashmore\n";
+        Thread.sleep(1500);
 
-    Thread.sleep(1500);
+        print("I just spent way too much money. Why did I do that?" +
+                "\n                 - Lance Douglas (Hey I know him!)");
 
-    String lDouglas = "I just spent way too much money. Why did I do that?" +
-            "\n                 - Lance Douglas (Hey I know him!)";
+        Thread.sleep(1500);*/
 
-    Thread.sleep(1500);*/
 
 
 
---
+++ COS312/HMWK1/src/QuotesP2.java
@@ -0,0 +1,70 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener; //Imported a bunch of JFrame stuff. Not totally sure.
+
+class Window extends JFrame {
+    JPanel frame = new JPanel();
+
+    String[] author = {"Select Choice","Nelson Mandela","Shawn Ashmore","John Richardson","Ben Franklin","Lance Douglas"};
+    String[] quotes = {"","Education is the most powerful weapon which you can use to change the world.",
+            "Style is a reflection of your attitude and your personality.","They can be silly, they can be political, whatever.",
+            "An investment in knowledge pays the best interest.","I just spent way too much money. Why did I do that?"};
+
+    JTextArea lbl = new JTextArea(50,30);
+
+    JComboBox Spinner = new JComboBox(author);
+
+    private Window() {
+        super("Quotes");
+        setSize(500, 250);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        add(frame);
+        setVisible(true);
+        Color customColor = new Color(2,84,82);
+        setBackground(customColor);
+
+        Spinner.setSelectedIndex(0);
+        frame.add(Spinner, BorderLayout.NORTH);
+        frame.add(lbl, BorderLayout.SOUTH);
+
+        lbl.setWrapStyleWord(true);
+        lbl.setLineWrap(true);
+        lbl.setOpaque(false);
+        lbl.setEditable(false);
+        lbl.setFocusable(false);
+        lbl.setBackground(UIManager.getColor("Label.background"));
+        lbl.setFont(UIManager.getFont("Label.font"));
+        lbl.setBorder(UIManager.getBorder("Label.border"));
+
+        Spinner.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+
+                JComboBox comboBox = (JComboBox) event.getSource();
+
+                Object selected = comboBox.getSelectedItem();
+                if(selected.toString().equals(author[0]))
+                    lbl.setText(quotes[0]);
+                else if(selected.toString().equals(author[1]))
+                    lbl.setText(quotes[1]);
+                else if(selected.toString().equals(author[2]))
+                    lbl.setText(quotes[2]);
+                else if(selected.toString().equals(author[3]))
+                    lbl.setText(quotes[3]);
+                else if(selected.toString().equals(author[4]))
+                    lbl.setText(quotes[4]);
+                else if(selected.toString().equals(author[5]))
+                    lbl.setText(quotes[5]);
+                else
+                    lbl.setText("You shouldn't be seeing this error message. If you are, contact the dev.");
+
+            }
+        });
+
+        setVisible(true);
+    }
+
+    public static void main (String[] args) {
+        Window gui = new Window();
+    }
+}
