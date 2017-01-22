f2f00d363f4c13876b100695e7989a7df50ca78f
Lance Douglas <ldouglastech@gmail.com>
2017-01-22 13:50:14 -0500

    Commented, learned, succeeded.

--- COS312/HMWK1/src/Quotes.java
+++ COS312/HMWK1/src/Quotes.java
@@ -1,29 +1,35 @@
+/* Alright, so Quotes is a simple (well, originally simple) class designed to take
+    three quotes and print them out, crediting the author each time. Easy peasy.
+    I spiced it up.
+    Five quotes, timed output, and my favorite, randomized output with no repeats.
+
+    Lance Douglas, HMWK1 Quotation Part 1, 1/29/2017
+ */
+
 import java.util.*;
 
 public class Quotes {
-
     public static void main(String[] args) throws InterruptedException {
 
-
-        String quotesList[] = {"Education is the most powerful weapon which you can use to change the world.\n\t\t- Nelson Mandela",
-                "They can be silly, they can be political, whatever.\n\t\t- John Richardson",
+        String quotesList[] = {"Education is the most powerful weapon which you can use to change the world.\n\t\t- Nelson Mandela", //\n Means New line,
+                "They can be silly, they can be political, whatever.\n\t\t- John Richardson", //and \t is tab, so adds newline and eight spaces.
                 "An investment in knowledge pays the best interest.\n\t\t- Ben Franklin",
                 "Style is a reflection of your attitude and your personality.\n\t\t- Shawn Ashmore",
-                "I just spent way too much money. Why did I do that?\n\t\t- Lance Douglas"};
+                "I just spent way too much money. Why did I do that?\n\t\t- Lance Douglas"}; //Array of all five quotes, containing quote and author.
 
-        Collections.shuffle(Arrays.asList(quotesList));
-
-        int quoteNum = 4;
+        Collections.shuffle(Arrays.asList(quotesList)); //This was annoying. It takes the array and shuffles
+                                                        //the order of the elements, so as to ensure I don't
+                                                        //repeat any pieces. "Shawn, Shawn, Nelson" for example.
+        int quoteNum = 4; //Just a simple loop to go through all five quotes.
         while (quoteNum >= 0) {
-            print(quotesList[quoteNum]);
-            Thread.sleep(1500);
-            quoteNum--;
+            print(quotesList[quoteNum]); //Prints out the quote at array[Index], which has been shuffled.
+            Thread.sleep(1500); //Wait 1.5 seconds for execution so I can read each quote.
+            quoteNum--; //Decrement quoteNum.
         }
     }
-
     private static void print(String s) {
-
-            System.out.println(s);
+            System.out.println(s); //Nothing new here, too lazy for Sys.out.print.
     }
 }
 
+//I really liked this assignment.
--- COS312/HMWK1/src/QuotesP2.java
+++ COS312/HMWK1/src/QuotesP2.java
@@ -1,72 +1,98 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener; //Imported a bunch of JFrame stuff. Not totally sure.
+/* So, this assignment is to take Quotation Part 1 and turn it into a GUI.
+    I was not prepared for the coming hardship.
 
-class Window extends JFrame {
-    JPanel frame = new JPanel();
+    Instead of following the PDF, I used a Spinner(JComboBox? Spinner in Android Development JCB/Spinner are interchangeable below.)
+    When each author within the JCB is selected, it displays the corresponding quote.
+    Also, it is colored and animated. I am really proud of this, can I have extra credit or something?
+    The background is a pastel teal with a crisp white Roboto font at the foreground 
+                                - Hipster web dev who uses a .io site. 
+                                
+    Lance Douglas, HMWK1 Quotation Part 2, 1/29/2017
 
-    String[] author = {"Select Choice","Nelson Mandela","Shawn Ashmore","John Richardson","Ben Franklin","Lance Douglas"};
+*/
+import javax.swing.*; //For GUI
+import java.awt.*; //For JFrame
+import java.awt.event.ActionEvent; //For buttons.
+import java.awt.event.ActionListener; //So button clicking can happen.
+
+class QuotesP2 extends JFrame { //Extends JFrame stuff.
+    JPanel frame = new JPanel(); //Creates new JFrame
+
+    String[] author = {"Select Choice","Nelson Mandela","Shawn Ashmore","John Richardson","Ben Franklin","Lance Douglas"}; //Author
     String[] quotes = {"","Education is the most powerful weapon which you can use to change the world.",
-            "Style is a reflection of your attitude and your personality.","They can be silly, they can be political, whatever.",
+            "Style is a reflection of your attitude and your personality.","They can be silly, they can be political, whatever.", //Quote; corresponds to author.
             "An investment in knowledge pays the best interest.","I just spent way too much money. Why did I do that?"};
 
-    JTextArea lbl = new JTextArea(30,30);
+    JTextArea tA = new JTextArea(30,20); //Set text area.
+    /* I used a text area because I couldn't word wrap a generic JLabel.
+        I made it non editable, and set the colors to match. Stealth.
+     */
 
-    JComboBox Spinner = new JComboBox(author);
+    JComboBox Spinner = new JComboBox(author); //Made a spinner / JCB / whatever.
+    // Intellij is mad at me though. Unchecked call? I dunno. Works fine.
 
-    private Window() {
-        super("Quotes");
-        setSize(400, 250);
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        add(frame);
-        setVisible(true);
-        Color customColor = new Color(2,84,82);
-        setBackground(customColor);
+    private QuotesP2() { //Method for JFRame
+        super("Quotes"); //Title of box.
+        setSize(400, 250); //Size of box in px.
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); //Destroy box on close.
+        add(frame); //Adds frame to box
+        setVisible(true); //Can't be invisible, obviously.
+        Color teal = new Color(0,150,136); //Almost my favorite color!
+        Color white = new Color(255,255,255); //Color.white is just too mainstream.
 
-        Spinner.setSelectedIndex(0);
-        frame.add(Spinner, BorderLayout.NORTH);
-        frame.add(lbl, BorderLayout.SOUTH);
-        frame.add(lbl, BorderLayout.CENTER);
+        //Instructions for Frame.
 
-        lbl.setWrapStyleWord(true);
-        lbl.setLineWrap(true);
-        lbl.setOpaque(false);
-        lbl.setEditable(false);
-        lbl.setFocusable(false);
-        lbl.setBackground(UIManager.getColor("Label.background"));
-        lbl.setFont(UIManager.getFont("Label.font"));
-        lbl.setBorder(UIManager.getBorder("Label.border"));
-        lbl.setFont(new Font("Ubuntu", Font.PLAIN, 16));
+        frame.add(Spinner, BorderLayout.NORTH); //Puts the JCB at the top of the Frame.
+        frame.add(tA, BorderLayout.SOUTH); //Puts the text area to the south of the Spinner.
+        frame.add(tA, BorderLayout.CENTER); //Centers the textbox
+        frame.setForeground(teal); //I like teal
+        frame.setBackground(teal); //For consistency.
 
-        Spinner.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent event) {
+        //Instructions for Text Area.
+        tA.setWrapStyleWord(true); //See? Can't word wrap a JLabel, and StackOverflow html tags did nothing.
+        tA.setLineWrap(true);
+        tA.setOpaque(false);
+        tA.setEditable(false); //No editing for posterity.
+        tA.setFocusable(false); //Can't highlight the text. Why? Practice.
+        tA.setBackground(teal); //Background color.
+        tA.setForeground(white); //Text color.
+        tA.setFont(new Font("Roboto", Font.BOLD, 16)); //Just experimenting with constraints.
 
-                JComboBox comboBox = (JComboBox) event.getSource();
+        //Instructions for Spinner.
 
-                Object selected = comboBox.getSelectedItem();
-                if(selected.toString().equals(author[0]))
-                    lbl.setText(quotes[0]);
+        Spinner.setForeground(white); //Sets text color in JCB to white, for contrast.
+        Spinner.setBackground(teal); //Sets the JCB background to teal to match the JFrame.
+        Spinner.setFocusable(false); //Oooh I figured this out myself. Can't scroll with keys now. Broke my implementation.
+        Spinner.setSelectedIndex(0); //Predetermine where Spinner spawns
+        Spinner.addActionListener(new ActionListener() { //IntelliJ is mad at me. Says AL call can be replaced with Lambda expression. Googled it. Even more confused.
+            //Anyways, puts AL on Spinner for clicking indexes.
+            public void actionPerformed(ActionEvent event) { //Thing to do on Click.
+
+                JComboBox comboBox = (JComboBox) event.getSource(); //I don't know what this does or what it's for.
+
+                Object selected = comboBox.getSelectedItem(); // Or this.
+                if(selected.toString().equals(author[0])) //If else to put matching quotes to author name in Spinner box.
+                    tA.setText(quotes[0]);
                 else if(selected.toString().equals(author[1]))
-                    lbl.setText(quotes[1]);
+                    tA.setText(quotes[1]);
                 else if(selected.toString().equals(author[2]))
-                    lbl.setText(quotes[2]);
+                    tA.setText(quotes[2]); //If Author two, use Quote two. Bad coding practices, but I don't know any better way.
                 else if(selected.toString().equals(author[3]))
-                    lbl.setText(quotes[3]);
+                    tA.setText(quotes[3]);
                 else if(selected.toString().equals(author[4]))
-                    lbl.setText(quotes[4]);
+                    tA.setText(quotes[4]);
                 else if(selected.toString().equals(author[5]))
-                    lbl.setText(quotes[5]);
+                    tA.setText(quotes[5]);
                 else
-                    lbl.setText("You shouldn't be seeing this error message. If you are, contact the dev.");
+                    tA.setText("You shouldn't be seeing this error message. If you are, I have no idea."); //This shouldn't appear.
 
             }
         });
 
-        setVisible(true);
+        setVisible(true); //Can't have a frackin invisible window.
     }
 
     public static void main (String[] args) {
-        Window gui = new Window();
-    }
+        Window gui = new QuotesP2();
+    } //This Initializes the GUI
 }
