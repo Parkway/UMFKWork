3786c9a87ef999db26b2903ee3be76e1beb0477a
Parkway <ldouglastech@gmail.com>
2017-02-10 12:16:08 -0500

    Added more hashing algorithms.

--- COS312/Capstone/src/cryptoEngine.java
+++ COS312/Capstone/src/cryptoEngine.java
@@ -14,12 +14,16 @@ public class cryptoEngine {
         Scanner scn = new Scanner(System.in);
         System.out.println("Hi there!\n" +
                 "This program is designed to take a String," +
-                "\nAnd return several different hashes of it.");
+                "\nAnd return several different hashes of it." +
+                "\nPlease enter your string:");
 
         String ogString = scn.nextLine();
 
         md5(ogString);
-        sha(ogString);
+        Thread.sleep(500);
+        sha512(ogString);
+        Thread.sleep(500);
+        sha1(ogString);
 
         again();
 
@@ -30,19 +34,36 @@ public class cryptoEngine {
             m.update(ogString.getBytes(), 0, ogString.length());
             System.out.println("MD5: " + new BigInteger(1, m.digest()).toString(16));
         } catch (NoSuchAlgorithmException NSAE) {
-
+            //Not empty nowwwww.
         }
     }
-    private static void sha (String ogString) {
-        //TODO Generate SHA algorithms.
+    private static void sha512 (String ogString) {
+        try {
+            MessageDigest m = MessageDigest.getInstance("SHA-512");
+            m.update(ogString.getBytes(), 0, ogString.length());
+            System.out.println("SHA-512: " + new BigInteger(1, m.digest()).toString(16));
+        } catch (NoSuchAlgorithmException NSAE) {
+            //Not empty nowwww.
+        }
     }
-    private static void again() {
+    private static void sha1 (String ogString) {
+        try {
+            MessageDigest m = MessageDigest.getInstance("SHA-1");
+            m.update(ogString.getBytes(), 0, ogString.length());
+            System.out.println("SHA-1: " + new BigInteger(1, m.digest()).toString(16));
+        } catch (NoSuchAlgorithmException NSAE) {
+            //Fuk u
+        }
+    }
+    private static void again() throws Exception{
         Scanner scn = new Scanner(System.in);
         System.out.println("Would you like to convert another string?");
         String repeat = scn.nextLine().toLowerCase();
         if (repeat.contains("y")) {
             System.out.println("Alright, another!");
-
+            stringer();
+        } else {
+            System.out.println("Okay byeeee!");
         }
     }
 }
